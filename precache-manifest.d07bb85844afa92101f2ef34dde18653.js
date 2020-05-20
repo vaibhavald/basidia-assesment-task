bd386d3d04972d8d12158018f28ff5bd8e53fae7
vaibhavald <vaibhavsrivastavaald@gmail.com>
2020-05-20 17:30:48 +0530

    Updates

---
+++ precache-manifest.d07bb85844afa92101f2ef34dde18653.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f2d1ffce92912c94f839382a2288763",
+    "url": "/basidia-assesment-task/index.html"
+  },
+  {
+    "revision": "9a9222194e52144a72c1",
+    "url": "/basidia-assesment-task/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ea0fe459a944f29cebad",
+    "url": "/basidia-assesment-task/static/css/main.8a561238.chunk.css"
+  },
+  {
+    "revision": "9a9222194e52144a72c1",
+    "url": "/basidia-assesment-task/static/js/2.bc1a43dd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/basidia-assesment-task/static/js/2.bc1a43dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea0fe459a944f29cebad",
+    "url": "/basidia-assesment-task/static/js/main.347a477c.chunk.js"
+  },
+  {
+    "revision": "5a0c8ea16b9bef667409",
+    "url": "/basidia-assesment-task/static/js/runtime-main.130d8809.js"
+  },
+  {
+    "revision": "0009b771b4369ffca3cdf45a30253da2",
+    "url": "/basidia-assesment-task/static/media/background-weather.0009b771.jpg"
+  }
+]);
